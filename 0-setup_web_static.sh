6549406e0eab0a495372d885eec14832f7387f24
uwen-godwin <uwengodwin001gmail.com>
2024-04-03 13:23:33 -0700

    Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,25 +13,31 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Change permissions
 sudo chown -hR ubuntu:ubuntu /data
 
-printf %s "server {
+# Writing Nginx configuration
+cat <<EOF > /etc/nginx/sites-available/default
+server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By $HOSTNAME;
     root   /var/www/html;
     index  index.html index.htm;
+
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
+
     location /redirect_me {
         return 301 http://cuberule.com/;
     }
+
     error_page 404 /404.html;
     location /404 {
         root /var/www/html;
-	internal;
+        internal;
     }
-}" > /etc/nginx/sites-available/default
+}
+EOF
 
-# Restart Nginx
-sudo service nginx restart
+# Reload Nginx configuration
+sudo service nginx reload
